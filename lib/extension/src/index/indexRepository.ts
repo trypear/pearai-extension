f92ada53c85b63943ef606ffa601ca93f6d97aab
EnderGamingFilms <endergamingfilms@gmail.com>
2024-05-19 22:55:03 -0700

    start yarn conversion

--- lib/extension/src/index/indexRepository.ts
+++ lib/extension/src/index/indexRepository.ts
@@ -157,7 +157,7 @@ function isSupportedFile(file: string) {
     !(
       file.endsWith(".min.js") ||
       file.endsWith(".min.css") ||
-      file.endsWith("pnpm-lock.yaml")
+      file.endsWith("yarn-lock.yaml")
     )
   );
 }
