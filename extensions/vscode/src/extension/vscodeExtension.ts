94ee089121e6b9c04fc05b476e88f2bff8fa7bc8
Nathan A <nang@nathans-mbp-8.mynetworksettings.com>
2024-06-13 16:33:09 -0400

    Renamed ContinueProxy

--- extensions/vscode/src/extension/vscodeExtension.ts
+++ extensions/vscode/src/extension/vscodeExtension.ts
@@ -1,5 +1,5 @@
 import { ConfigHandler } from "core/config/handler";
-import { ContinueServerClient } from "core/continueServer/stubs/client";
+import { PearAIServerClient } from "core/continueServer/stubs/client";
 import { CodebaseIndexer, PauseToken } from "core/indexing/indexCodebase";
 import { IdeSettings } from "core/protocol";
 import { getConfigJsonPath, getConfigTsPath } from "core/util/paths";
@@ -65,7 +65,7 @@ export class VsCodeExtension {
       },
     );
 
-    const continueServerClient = new ContinueServerClient(
+    const PearAIServerClient = new PearAIServerClient(
       ideSettings.remoteConfigServerUrl,
       userTokenPromise,
     );
@@ -142,7 +142,7 @@ export class VsCodeExtension {
       this.configHandler,
       this.ide,
       indexingPauseToken,
-      continueServerClient,
+      PearAIServerClient,
     );
 
     if (
