e0de0e90135c3466d4c9aff770756eb799afa7b9
Aura <coolboy1384@gmail.com>
2024-06-17 14:55:09 -0400

    vscode setting migration v2
    fixed some fixes from the previous commit
    correctly moves settings json and all relevant vscode settings to .pearai-dev

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -5,12 +5,23 @@ import * as path from 'path';
 import * as os from 'os';
 import { getExtensionVersion } from "./util/util";
 
-const pearAISettingsDir = path.join(os.homedir(), '.pearai');
+const pearAISettingsDir = path.join(os.homedir(), '.pearai-dev');
 const firstLaunchFlag = path.join(pearAISettingsDir, 'firstLaunch.flag');
 
+function getPearAIDevSettingsDir() {
+  const platform = process.platform;
+  if (platform === 'win32') {
+    return path.join(process.env.APPDATA || '', 'pearai-dev', 'User');
+  } else if (platform === 'darwin') {
+    return path.join(os.homedir(), 'Library', 'Application Support', 'pearai-dev', 'User');
+  } else {
+    return path.join(os.homedir(), '.config', 'pearai-dev', 'User');
+  }
+}
+
 async function dynamicImportAndActivate(context: vscode.ExtensionContext) {
-  const { activateExtension } = await import("./activation/activate");
   try {
+    const { activateExtension } = await import("./activation/activate");
     await activateExtension(context);
   } catch (e) {
     console.log("Error activating extension: ", e);
@@ -31,10 +42,43 @@ async function dynamicImportAndActivate(context: vscode.ExtensionContext) {
   }
 }
 
+function promptUserToCopySettings() {
+  vscode.window.showInformationMessage('Do you want to copy your current VSCode settings to the PearAI settings directory?', 'Yes', 'No')
+    .then(selection => {
+      if (selection === 'Yes') {
+        copyVSCodeSettingsToPearAIDir();
+      }
+      fs.writeFileSync(firstLaunchFlag, 'This is the first launch flag file');
+    });
+}
+
+function copyVSCodeSettingsToPearAIDir() {
+  const vscodeSettingsDir = getVSCodeSettingsDir();
+  const pearAIDevSettingsDir = getPearAIDevSettingsDir();
+
+  if (!fs.existsSync(pearAIDevSettingsDir)) {
+    fs.mkdirSync(pearAIDevSettingsDir, { recursive: true });
+  }
+
+  const itemsToCopy = ['settings.json', 'keybindings.json', 'snippets', 'sync'];
+  itemsToCopy.forEach(item => {
+    const source = path.join(vscodeSettingsDir, item);
+    const destination = path.join(pearAIDevSettingsDir, item);
+    if (fs.existsSync(source)) {
+      if (fs.lstatSync(source).isDirectory()) {
+        copyDirectoryRecursiveSync(source, destination);
+      } else {
+        fs.copyFileSync(source, destination);
+      }
+    }
+  });
+  vscode.window.showInformationMessage('Your VSCode settings have been copied to the PearAI settings directory.');
+}
+
 function getVSCodeSettingsDir() {
   const platform = process.platform;
   if (platform === 'win32') {
-    return path.join(os.homedir(), 'AppData', 'Roaming', 'Code', 'User');
+    return path.join(process.env.APPDATA || '', 'Code', 'User');
   } else if (platform === 'darwin') {
     return path.join(os.homedir(), 'Library', 'Application Support', 'Code', 'User');
   } else {
@@ -42,34 +86,19 @@ function getVSCodeSettingsDir() {
   }
 }
 
-function promptUserToCopySettings() {
-  vscode.window.showInformationMessage('Do you want to copy your current VSCode settings to PearAI?', 'Yes', 'No')
-    .then(selection => {
-      if (selection === 'Yes') {
-        copyVSCodeSettingsToPearAI();
-      }
-      fs.writeFileSync(firstLaunchFlag, 'This is the first launch flag file');
-    });
-}
-
-function copyVSCodeSettingsToPearAI() {
-  const vscodeSettingsDir = getVSCodeSettingsDir();
-  const pearAIUserSettingsDir = path.join(pearAISettingsDir, 'User');
-
-  if (!fs.existsSync(pearAIUserSettingsDir)) {
-    fs.mkdirSync(pearAIUserSettingsDir, { recursive: true });
+function copyDirectoryRecursiveSync(source: string, destination: string) {
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination, { recursive: true });
   }
-
-  const filesToCopy = ['settings.json', 'keybindings.json', 'snippets'];
-  filesToCopy.forEach(file => {
-    const source = path.join(vscodeSettingsDir, file);
-    const destination = path.join(pearAIUserSettingsDir, file);
-    if (fs.existsSync(source)) {
-      fs.copyFileSync(source, destination);
+  fs.readdirSync(source).forEach(item => {
+    const sourcePath = path.join(source, item);
+    const destinationPath = path.join(destination, item);
+    if (fs.lstatSync(sourcePath).isDirectory()) {
+      copyDirectoryRecursiveSync(sourcePath, destinationPath);
+    } else {
+      fs.copyFileSync(sourcePath, destinationPath);
     }
   });
-
-  vscode.window.showInformationMessage('Your VSCode settings have been copied to PearAI.');
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -83,6 +112,5 @@ export function deactivate() {
   Telemetry.capture("deactivate", {
     extensionVersion: getExtensionVersion(),
   });
-
   Telemetry.shutdownPosthogClient();
 }
