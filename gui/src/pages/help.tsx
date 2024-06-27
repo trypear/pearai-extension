1caeca6ce1ce36b1a938f817fdca6c6498ebfa63
Mex777 <fologeaalexandru@gmail.com>
2024-06-27 13:20:39 +0000

    Changed help center links and shortcuts

--- gui/src/pages/help.tsx
+++ gui/src/pages/help.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import {
@@ -128,7 +128,7 @@ function HelpPage() {
           </a>
         </IconDiv>
         <IconDiv backgroundColor="rgb(88, 98, 227)">
-          <a href="https://discord.gg/vapESyrFmJ" target="_blank">
+          <a href="https://discord.gg/Uw9mVvFUk3" target="_blank">
             <svg
               width="42px"
               height="42px"
